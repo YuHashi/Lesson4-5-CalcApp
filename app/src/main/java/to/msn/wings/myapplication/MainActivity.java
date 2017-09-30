50c4293e9eac556c7a4e38ec1b7755326cea5055
YuHashi <lacrosse-otafuku@hotmail.co.jp>
2017-09-30 19:37:09 +0900

    課題提出_4-5_四則計算アプリ_初回コミット

---
+++ app/src/main/java/to/msn/wings/myapplication/MainActivity.java
@@ -0,0 +1,63 @@
+package to.msn.wings.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText editText1;
+    EditText editText2;
+    int value1;
+    int value2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonSum = (Button) findViewById(R.id.buttonSum);
+        buttonSum.setOnClickListener(this);
+
+        Button buttonMin = (Button) findViewById(R.id.buttonMin);
+        buttonMin.setOnClickListener(this);
+
+        Button buttonMul = (Button) findViewById(R.id.buttonMul);
+        buttonMul.setOnClickListener(this);
+
+        Button buttonDiv = (Button) findViewById(R.id.buttonDiv);
+        buttonDiv.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v){
+        editText1 = (EditText) findViewById(R.id.editText1);
+        editText2 = (EditText) findViewById(R.id.editText2);
+
+        String mEditText1 = editText1.getText().toString();
+        String mEditText2 = editText2.getText().toString();
+
+        value1 = Integer.parseInt(mEditText1);
+        value2 = Integer.parseInt(mEditText2);
+
+        int calc = 0;
+        if (v.getId()==R.id.buttonSum){
+            calc = value1 + value2;
+        } else if (v.getId()==R.id.buttonMin){
+            calc = value1 - value2;
+        } else if (v.getId()==R.id.buttonMul){
+            calc = value1 * value2;
+        } else if (v.getId()==R.id.buttonDiv) {
+            calc = value1 / value2;
+        }
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("Result", calc);
+        startActivity(intent);
+    }
+}
